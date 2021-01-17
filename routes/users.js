8fcf23514608499c09c6ee4a396cd8a865ec7ac0
putrasatr <satriaputrapratama2002@gmail.com>
2021-01-17 19:31:12 +0700

    22 (First)

---
+++ routes/users.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+var MongoClient = require('mongodb').MongoClient
+
+var state = {
+    db: null,
+}
+
+exports.connect = function (url, done) {
+    if (state.db) return done()
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) return done(err)
+        state.db = db
+        done()
+    })
+}
+
+
+exports.get = function () {
+    return state.db
+}
+
+exports.close = function (done) {
+    if (state.db) {
+        state.db.close(function (err, result) {
+            state.db = null
+            state.mode = null
+            done(err)
+        })
+    }
+}
+
+module.exports = router;
